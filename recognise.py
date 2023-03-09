ad159e0d77eacff9a1258a61fe652ce43b85f3f1
Paul Artushkov <pavTiger@gmail.com>
2023-03-10 00:35:47 +0300

    Use cv2.minAreaRect to build rectangle around contour even if it's rotated

--- recognise.py
+++ recognise.py
@@ -109,6 +109,61 @@ def clear_directory(path):
         os.remove(f)
 
 
+def order_points(pts):
+    # Initialize a list of coordinates that will be ordered
+    # such that the first entry in the list is the top-left,
+    # the second entry is the top-right, the third is the
+    # bottom-right, and the fourth is the bottom-left
+    rect = np.zeros((4, 2), dtype = "float32")
+    # the top-left point will have the smallest sum, whereas
+    # the bottom-right point will have the largest sum
+    s = pts.sum(axis = 1)
+    rect[0] = pts[np.argmin(s)]
+    rect[2] = pts[np.argmax(s)]
+    # now, compute the difference between the points, the
+    # top-right point will have the smallest difference,
+    # whereas the bottom-left will have the largest difference
+    diff = np.diff(pts, axis = 1)
+    rect[1] = pts[np.argmin(diff)]
+    rect[3] = pts[np.argmax(diff)]
+    # return the ordered coordinates
+    return rect
+
+
+def four_point_transform(image, pts):
+    # obtain a consistent order of the points and unpack them
+    # individually
+    rect = order_points(pts)
+    (tl, tr, br, bl) = rect
+    # compute the width of the new image, which will be the
+    # maximum distance between bottom-right and bottom-left
+    # x-coordiates or the top-right and top-left x-coordinates
+    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+    maxWidth = max(int(widthA), int(widthB))
+    # compute the height of the new image, which will be the
+    # maximum distance between the top-right and bottom-right
+    # y-coordinates or the top-left and bottom-left y-coordinates
+    heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+    heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+    maxHeight = max(int(heightA), int(heightB))
+    # now that we have the dimensions of the new image, construct
+    # the set of destination points to obtain a "birds eye view",
+    # (i.e. top-down view) of the image, again specifying points
+    # in the top-left, top-right, bottom-right, and bottom-left
+    # order
+    dst = np.array([
+        [0, 0],
+        [maxWidth - 1, 0],
+        [maxWidth - 1, maxHeight - 1],
+        [0, maxHeight - 1]], dtype = "float32")
+    # compute the perspective transform matrix and then apply it
+    M = cv2.getPerspectiveTransform(rect, dst)
+    warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+    # return the warped image
+    return warped
+
+
 def detect_table(filename, page, prefix_path):
     im1 = cv2.imread(filename, 0)
     im = cv2.imread(filename)
@@ -124,18 +179,34 @@ def detect_table(filename, page, prefix_path):
     contours, _ = cv2.findContours(dilated_value, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
     max_space = 0
-    table_coords = Rect(0, 0, 0, 0)
+    table_coords = None
     for cnt in contours:
         x, y, w, h = cv2.boundingRect(cnt)
+        rect = cv2.minAreaRect(cnt)
+
         # Bounding the images
         if (w * h) > (shape[0] * shape[1] / 15) and (w * h) > max_space:
             max_space = (w * h)
-            table_coords = Rect(x, y, w, h)
+            table_coords = rect
 
-    cv2.rectangle(im, (table_coords.x, table_coords.y),
-                  (table_coords.x + table_coords.w, table_coords.y + table_coords.h), (0, 0, 255), 5)
+    if table_coords is None:
+        return None
+
+    # Draw table rectangle on top of the image for debug purposes
+    box = cv2.boxPoints(table_coords)
+    box_points = []
+    for i in range(4):
+        box_points.append((int(box[i][0]), int(box[i][1])))
+
+    crop = four_point_transform(im, box)
+
+    cv2.line(im, box_points[0], box_points[1], (0, 255, 0), 3)
+    cv2.line(im, box_points[1], box_points[2], (0, 255, 0), 3)
+    cv2.line(im, box_points[2], box_points[3], (0, 255, 0), 3)
+    cv2.line(im, box_points[3], box_points[0], (0, 255, 0), 3)
     cv2.imwrite(f'{prefix_path}output/detects/detect_table_{page}.jpg', im)
-    return table_coords
+
+    return crop
 
 
 async def process(prefix_path, pdf_file, quality, limit, capture_stdout, sid=None, sio=None, user_connected=None):
@@ -165,15 +236,15 @@ async def process(prefix_path, pdf_file, quality, limit, capture_stdout, sid=Non
         image_path = f'{prefix_path}output/pages/page_{page_index}.jpg'
         pix.save(image_path)
 
-        detected_cont = detect_table(image_path, page_index, prefix_path)
+        detected_cropped = detect_table(image_path, page_index, prefix_path)
 
-        if detected_cont != Rect(0, 0, 0, 0):
-            image = cv2.imread(image_path)
-            cropped = image[detected_cont.y:detected_cont.y + detected_cont.h,
-                      detected_cont.x:detected_cont.x + detected_cont.w]
+        if detected_cropped is not None:
+            # image = cv2.imread(image_path)
+            # cropped = image[detected_cont.y:detected_cont.y + detected_cont.h,
+            #           detected_cont.x:detected_cont.x + detected_cont.w]
 
             cropped_filename = f"{prefix_path}output/cropped/cropped_table_{page_index + 1}.jpg"
-            cv2.imwrite(cropped_filename, cropped)
+            cv2.imwrite(cropped_filename, detected_cropped)
 
             if capture_stdout:
                 # Send current page to the user
